46ee33df0bfc101a9fa8ee626fca0fe122d0de04
Daniel Scott-Raynsford <plague.ho@gmail.com>
2015-08-21 11:24:19 +1200

    Restructure project

--- Test/LabBuilder.Test.Default.ps1
+++ Test/LabBuilder.Test.Default.ps1
@@ -1,11 +1,11 @@
 ﻿##########################################################################################################################################
 Function Test-LabBuilderInstall {
-	Install-Lab -Path "$PSScriptRoot\SampleConfig.xml" -Verbose
+	Install-Lab -Path "$PSScriptRoot\TestConfig1.xml" -Verbose
 } # Function Test-LabBuilderInstall
 ##########################################################################################################################################
 Function Test-LabBuilderLoadModule {
 	Get-Module | Where-Object -Property Name -Eq LabBuilder | Remove-Module
-	Import-Module "$PSScriptRoot\LabBuilder" -Verbose
+	Import-Module "$PSScriptRoot\..\LabBuilder" -Verbose
 } # Function Test-LabBuilderLoadModule
 ##########################################################################################################################################
 Test-LabBuilderLoadModule
