b02ff5d39d122de26bfe8ec9f2daf2e24ddf46ed
Daniel Scott-Raynsford <plagueho@gmail.com>
2015-08-31 19:46:41 +1200

    US12: Additional Test Configuratio VMs added

---
+++ LabBuilder/Tests/TestConfig1/PRIMARYDC.DSC.ps1
@@ -0,0 +1,43 @@
+Configuration ROOTCA
+{
+	Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
+	Import-DscResource -ModuleName xActiveDirectory 
+	Node $AllNodes.NodeName {
+		# Assemble the Local Admin Credentials
+		If ($Node.LocalAdminPassword) {
+			[PSCredential]$LocalAdminCredential = New-Object System.Management.Automation.PSCredential ("Administrator", (ConvertTo-SecureString $Node.LocalAdminPassword -AsPlainText -Force))
+		}
+		If ($Node.DomainAdminPassword) {
+			[PSCredential]$DomainAdminCredential = New-Object System.Management.Automation.PSCredential ("Administrator", (ConvertTo-SecureString $Node.DomainAdminPassword -AsPlainText -Force))
+		}
+
+		WindowsFeature DNSInstall 
+        { 
+            Ensure = "Present" 
+            Name = "DNS" 
+        } 
+
+		WindowsFeature ADDSInstall 
+        { 
+            Ensure = "Present" 
+            Name = "AD-Domain-Services" 
+			DependsOn = "[WindowsFeature]DNSInstall" 
+        } 
+
+        xADDomain PrimaryDC 
+        { 
+            DomainName = $Node.DomainName 
+            DomainAdministratorCredential = $DomainAdminCredential 
+            SafemodeAdministratorPassword = $LocalAdminCredential 
+            DependsOn = "[WindowsFeature]ADDSInstall" 
+        } 
+
+        xWaitForADDomain DscForestWait 
+        { 
+            DomainName = $Node.DomainName 
+            DomainUserCredential = $DomainAdminCredential 
+            DependsOn = "[xADDomain]PrimaryDC" 
+        } 
+
+	}
+}
