063dca5f8f11a079b8dac4d6e99d5513f0d0494c
Daniel Scott-Raynsford <plagueho@gmail.com>
2015-09-04 08:51:56 +1200

    Test added

--- LabBuilder/Tests/TestConfig1/ROOTCA.DSC.ps1
+++ LabBuilder/Tests/TestConfig1/ROOTCA.DSC.ps1
@@ -42,13 +42,13 @@ Configuration ROOTCA
 		{
 			SetScript = {
 				If ($Node.DSConifgDN) {
-					& "certutil.exe -setreg CA\DSConfigDN `"$($Node.DSConfigDN)`"" *>> c:\windows\setup\scripts\certutil.log
+					& "certutil.exe -setreg CA\DSConfigDN `"$($Node.DSConfigDN)`""
 				}
 				If ($Node.CRLPublicationURLs) {
-					& "certutil.exe -setreg CA\CRLPublicationURLs `"$($Node.CRLPublicationURLs)`"" *>> c:\windows\setup\scripts\certutil.log
+					& "certutil.exe -setreg CA\CRLPublicationURLs `"$($Node.CRLPublicationURLs)`""
 				}
 				If ($Node.CACertPublicationURLs) {
-					& "certutil.exe -setreg CA\CACertPublicationURLs `"$($Node.CACertPublicationURLs)`"" *>> c:\windows\setup\scripts\certutil.log
+					& "certutil.exe -setreg CA\CACertPublicationURLs `"$($Node.CACertPublicationURLs)`""
 				}
 				Restart-Service -Name CertSvc
 				Add-Content -Path 'c:\windows\setup\scripts\certutil.log' -Value "Certificate Service Restarted ..."
@@ -61,13 +61,19 @@ Configuration ROOTCA
 				}
 			}
 			TestScript = { 
+				Add-Content -Path "$ENV:Temp\TestScript.log" -Value ((Get-ChildItem 'HKLM:\System\CurrentControlSet\Services\CertSvc\Configuration').GetValue('DSConfigDN'))
 				If (($Node.DSConfigDN) -and ((Get-ChildItem 'HKLM:\System\CurrentControlSet\Services\CertSvc\Configuration').GetValue('DSConfigDN') -ne $Node.DSConfigDN)) {
+					Add-Content -Path "$ENV:Temp\TestScript.log" -Value "DSConfigDN NE"
 					Return $False
 				}
+				Add-Content -Path "$ENV:Temp\TestScript.log" -Value ((Get-ChildItem 'HKLM:\System\CurrentControlSet\Services\CertSvc\Configuration').GetValue('CRLPublicationURLs'))
 				If (($Node.CRLPublicationURLs) -and ((Get-ChildItem 'HKLM:\System\CurrentControlSet\Services\CertSvc\Configuration').GetValue('CRLPublicationURLs') -ne $Node.CRLPublicationURLs)) {
+					Add-Content -Path "$ENV:Temp\TestScript.log" -Value "CRLPublicationURLs NE"
 					Return $False
 				}
+				Add-Content -Path "$ENV:Temp\TestScript.log" -Value ((Get-ChildItem 'HKLM:\System\CurrentControlSet\Services\CertSvc\Configuration').GetValue('CACertPublicationURLs'))
 				If (($Node.CACertPublicationURLs) -and ((Get-ChildItem 'HKLM:\System\CurrentControlSet\Services\CertSvc\Configuration').GetValue('CACertPublicationURLs') -ne $Node.CACertPublicationURLs)) {
+					Add-Content -Path "$ENV:Temp\TestScript.log" -Value "CACertPublicationURLs NE"
 					Return $False
 				}
 				Return $True
