3b0d3f46894dc92fe80b2b3d5a4e0dc0b0555d61
Daniel Scott-Raynsford <plagueho@gmail.com>
2015-09-07 12:02:09 +1200

    StartDSC.ps1 no longer invoked as Job

--- LabBuilder/LabBuilder.psm1
+++ LabBuilder/LabBuilder.psm1
@@ -1004,7 +1004,7 @@ function Start-LabVMDSC {
 		# Finally, Start DSC up!
 		If (($Session) -and ($Session.State -eq 'Opened') -and ($ConfigCopyComplete) -and ($ModuleCopyComplete)) {
 			Write-Verbose "Starting DSC on VM $($VM.ComputerName) ..."
-			Invoke-Command -Session $Session { c:\windows\setup\scripts\StartDSC.ps1 } -AsJob
+			Invoke-Command -Session $Session { c:\windows\setup\scripts\StartDSC.ps1 }
 			Remove-PSSession -Session $Session		
 			$Complete = $True
 		} # If
