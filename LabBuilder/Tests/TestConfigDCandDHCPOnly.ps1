f1187f147c2272cbb52b089484c0e16c9ddb4873
Daniel Scott-Raynsford <plagueho@gmail.com>
2015-09-09 15:19:04 +1200

    Another fix to AppVeyor.yml artifact push

---
+++ LabBuilder/Tests/TestConfigDCandDHCPOnly.ps1
@@ -0,0 +1,17 @@
+﻿##########################################################################################################################################
+Function Test-LabBuilderInstall {
+	Install-Lab -Path "$PSScriptRoot\TestConfigDCandDHCPOnly.xml" -Verbose
+} # Function Test-LabBuilderInstall
+##########################################################################################################################################
+Function Test-LabBuilderUninstall {
+	Uninstall-Lab -Path "$PSScriptRoot\TestConfigDCandDHCPOnly.xml" -Verbose -RemoveVHDs -RemoveTemplates
+} # Function Test-LabBuilderUnnstall
+##########################################################################################################################################
+Function Test-LabBuilderLoadModule {
+	Import-Module "$PSScriptRoot\..\LabBuilder" -Verbose -Force
+} # Function Test-LabBuilderLoadModule
+##########################################################################################################################################
+Test-LabBuilderLoadModule
+Test-LabBuilderInstall
+# Sleep 30 # Wait 30 seconds for everything to finish building
+# Test-LabBuilderUninstall
