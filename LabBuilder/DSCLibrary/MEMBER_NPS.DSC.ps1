810baa5a628a13ca315919d52bc009b18b6d6eab
Daniel Scott-Raynsford <plagueho@gmail.com>
2015-09-21 19:59:28 +1200

    Additional features added to NPS in TestConfigFull.xml

--- LabBuilder/DSCLibrary/MEMBER_NPS.DSC.ps1
+++ LabBuilder/DSCLibrary/MEMBER_NPS.DSC.ps1
@@ -4,6 +4,8 @@ DSC Template Configuration File For use by LabBuilder
 	MEMBER_NPS
 .Desription
 	Builds a Server that is joined to a domain and then contains NPS/Radius components.
+.Requires
+	Windows Server 2012 R2 Full (Server core not supported).
 .Parameters:          
 	DomainName = "LABBUILDER.COM"
 	DomainAdminPassword = "P@ssword!1"
@@ -33,6 +35,13 @@ Configuration MEMBER_NPS
         { 
             Ensure = "Present" 
             Name = "NPAS-Health" 
+			DependsOn = "[WindowsFeature]NPASPolicyServerInstall" 
+        } 
+
+		WindowsFeature RSATNPAS
+        { 
+            Ensure = "Present" 
+            Name = "RSAT-NPAS" 
 			DependsOn = "[WindowsFeature]NPASPolicyServerInstall" 
         } 
 
