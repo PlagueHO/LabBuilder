1056a1fa085362609849fc82924a0227fb0dc66c
Daniel Scott-Raynsford <plagueho@gmail.com>
2016-02-16 19:28:53 +1300

    Correction to Unattend File tests

--- LabBuilder/Tests/Unit/LabBuilder.tests.ps1
+++ LabBuilder/Tests/Unit/LabBuilder.tests.ps1
@@ -1431,7 +1431,7 @@ InModuleScope LabBuilder {
             [Array]$Templates = Get-LabVMTemplates -Configuration $Config
             [Array]$VMs = Get-LabVMs -Configuration $Config -VMTemplates $Templates -Switches $Switches
             [String]$UnattendFile = Get-LabUnattendFile -Configuration $Config -VM $VMs[0]
-            Set-Content -Path "$($Global:ArtifactPath)\UnattendFile.xml" -Value $UnattendFile -Encoding UTF8 -NoNewLine
+            Set-Content -Path "$($Global:ArtifactPath)\ExpectedUnattendFile.xml" -Value $UnattendFile -Encoding UTF8 -NoNewLine
             It 'Returns Expected File Content' {
                 $UnattendFile | Should Be $True
                 $ExpectedUnattendFile = Get-Content -Path "$Global:TestConfigPath\ExpectedUnattendFile.xml" -Raw
