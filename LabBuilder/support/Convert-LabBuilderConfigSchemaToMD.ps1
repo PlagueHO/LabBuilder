7e5c6fb83e3645b489d159d686476f32f569e6ae
Daniel Scott-Raynsford <plagueho@gmail.com>
2016-02-29 11:45:34 +1300

    More work on XSD->MD

--- LabBuilder/support/Convert-LabBuilderConfigSchemaToMD.ps1
+++ LabBuilder/support/Convert-LabBuilderConfigSchemaToMD.ps1
@@ -6,7 +6,7 @@
 $XMLFile = Join-Path -Path $PSScriptRoot -ChildPath '..\schema\labbuilderconfig-schema.xsd'
 $XSLFile = Join-Path -Path $PSScriptRoot -ChildPath 'transform\labbuilderconfig-schema-transformtomd.xsl'
 $OutputFile = Join-Path -Path $PSScriptRoot -ChildPath '..\schema\labbuilderconfig-schema.md'
-.\Convert-XSDToMD.ps1 `
+& "$PSScriptRoot\Convert-XSDToMD.ps1" `
     -XmlFile $XMLFile `
     -XslFile $XSLFile `
     -OutputFile $OutputFile
