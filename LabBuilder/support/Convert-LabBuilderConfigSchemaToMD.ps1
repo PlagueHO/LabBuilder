23bfe80499c8b6683d3adfbcb3527205c3ae2e6b
Daniel Scott-Raynsford <plagueho@gmail.com>
2016-04-27 14:24:39 +1200

    * Added .vscode\tasks.json file to allow quick conversion of LabBuilder Schema to MD.

--- LabBuilder/support/Convert-LabBuilderConfigSchemaToMD.ps1
+++ LabBuilder/support/Convert-LabBuilderConfigSchemaToMD.ps1
@@ -6,6 +6,7 @@
 $XMLFile = Join-Path -Path $PSScriptRoot -ChildPath '..\schema\labbuilderconfig-schema.xsd'
 $XSLFile = Join-Path -Path $PSScriptRoot -ChildPath 'transform\labbuilderconfig-schema-transformtomd.xsl'
 $OutputFile = Join-Path -Path $PSScriptRoot -ChildPath '..\docs\labbuilderconfig-schema.md'
+Write-Verbose -Verbose "Conversion '..\schema\labbuilderconfig-schema.xsd' to '..\docs\labbuilderconfig-schema.md' started"
 & "$PSScriptRoot\Convert-XSDToMD.ps1" `
     -XmlFile $XMLFile `
     -XslFile $XSLFile `
