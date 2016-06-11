2be6c0300c4639c5c11bee688a7319eb121527b3
Daniel Scott-Raynsford <plagueho@gmail.com>
2016-06-11 19:25:47 +1200

    Added Initial Jenkins build support

---
+++ .jenkins/test.ps1
@@ -0,0 +1,16 @@
+Push-Location
+Set-Location -Path "$PSScriptRoot\..\LabBuilder\"
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+if (-not (Get-Module -Name Pester -ListAvailable -ErrorAction SilentlyContinue))
+{
+    Install-Module Pester -Force
+}
+Install-WindowsFeature -Name Hyper-V-PowerShell
+$testResultsFile = "$PSScriptRoot\..\LabBuilder\TestsResults.xml"
+Invoke-Pester
+    -OutputFormat NUnitXml `
+    -OutputFile $testResultsFile `
+    -PassThru `
+    -ExcludeTag Incomplete
+Pop-Location
