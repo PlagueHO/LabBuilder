d5cccd85362517427477fee17da77f0fe0614076
Daniel Scott-Raynsford <plagueho@gmail.com>
2016-09-30 23:03:27 +1300

    Prevent tests from being destructive

--- LabBuilder/tests/unit/lib/public/vmtemplate.tests.ps1
+++ LabBuilder/tests/unit/lib/public/vmtemplate.tests.ps1
@@ -1,5 +1,5 @@
 $Global:ModuleRoot = Resolve-Path -Path "$($Script:MyInvocation.MyCommand.Path)\..\..\..\..\..\"
-
+$OldPSModulePath = $env:PSModulePath
 Push-Location
 try
 {
@@ -361,4 +361,5 @@ catch
 finally
 {
     Pop-Location
+    $env:PSModulePath = $OldPSModulePath
 }
